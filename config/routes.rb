19ccdf194108f36af577346de435f2c2b9b530aa
John Williams <jwilliams722@gmail.com>
2012-12-10 18:21:52 -0700

    Finished portfolio application

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Portfolio::Application.routes.draw do
 
 
   get "log_out" => "sessions#destroy", :as => "log_out"
-  get "log_in" => "sessions#new", :as => "log_in"
+  get "admin" => "sessions#new", :as => "admin"
   get "sign_up" => "users#new", :as => "sign_up"
 
 
